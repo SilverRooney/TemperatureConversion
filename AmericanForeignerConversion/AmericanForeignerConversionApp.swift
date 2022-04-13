9ffe40040400df53a2e98a9784eac14bc3995cf9
SilverRooney <silver.rooney.dev@gmail.com>
2022-04-13 18:31:38 -0500

    Initial Commit

---
+++ AmericanForeignerConversion/AmericanForeignerConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmericanForeignerConversionApp.swift
+//  AmericanForeignerConversion
+//
+//  Created by Silver on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct AmericanForeignerConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
