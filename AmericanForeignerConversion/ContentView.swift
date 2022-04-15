2ea17b3db088eaa23c66ce6360fb201a48b230cf
SilverRooney <silver.rooney.dev@gmail.com>
2022-04-14 19:06:05 -0500

    first commit
    first commit

--- AmericanForeignerConversion/ContentView.swift
+++ AmericanForeignerConversion/ContentView.swift
@@ -8,10 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+           
+    
+    @State private var fahrenheit = 0.0
+    @State private var celsiusInput = 0.0
+    
+    var convertedToCelsius: Double {
+        let celsius = Double((fahrenheit - 32) * (5/9))
+        return celsius
     }
+    
+    var convertedToFarenheit: Double {
+        let farenheitConverted = Double((celsiusInput * (9/5)) + 32)
+        return farenheitConverted
+    }
+    
+    var body: some View {
+        
+        Form {
+            
+            //Fahrenheit to Celsius
+        
+            Section {
+                TextField("Fahrenheit", value: $fahrenheit, format: .number)
+                    .keyboardType(.decimalPad)
+            }
+        header: {
+            Text("Fahrenheit to Celsius")
+        }
+            
+            Section {
+                Text(convertedToCelsius, format: .number)
+            }
+            
+            
+  
+            //Celsius to Fahrenheit
+  
+            Section {
+                TextField("Celsius", value: $celsiusInput, format: .number)
+                    .keyboardType(.decimalPad)
+            }
+        header: {
+            Text("Celsius to Fahrenheit")
+        }
+            Section {
+                Text(convertedToFarenheit, format: .number)
+            }
+        
+        }
+    }
+
+    
+    
+
 }
 
 struct ContentView_Previews: PreviewProvider {
